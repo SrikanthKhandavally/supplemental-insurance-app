597ece9f02d314c9771bde4a442454355d1c8997
Srikanth Khandavally <Srikanth.Khandavally@express-scripts.com>
2025-06-16 10:39:55 -0500

    Initial commit - Supplemental Insurance Portal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PolicyComponent } from './components/policy/policy.component';
+import { ClientComponent } from './components/client/client.component';
+import { EnrollmentComponent } from './components/enrollment/enrollment.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'policies', component: PolicyComponent },
+  { path: 'clients', component: ClientComponent },
+  { path: 'enrollment', component: EnrollmentComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PolicyComponent } from './components/policy/policy.component';
+import { ClientComponent } from './components/client/client.component';
+import { EnrollmentComponent } from './components/enrollment/enrollment.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolicyComponent,
+    ClientComponent,
+    EnrollmentComponent,
+    DashboardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
